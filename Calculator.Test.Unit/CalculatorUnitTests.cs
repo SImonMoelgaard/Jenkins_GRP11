cb9b6f5790b072edcd54d21f997bc019b89ea91a
Simon Mølgaard Gertsen <au558684@uni.au.dk>
2021-02-10 09:18:03 +0100

    ff

--- Calculator.Test.Unit/CalculatorUnitTests.cs
+++ Calculator.Test.Unit/CalculatorUnitTests.cs
@@ -64,17 +64,49 @@ namespace Calculator.Test.Unit
 
 
         [TestCase(6, 2, 3)]
+        [TestCase(10, 2, 5)]
+        [TestCase(30, 6, 5)]
+        [TestCase(100, 10, 10)]
         public void Divide_ResultIsCorrect(double divideend, double divisor, double result)
         {
             Assert.That(_uut.Divide(divideend, divisor), Is.EqualTo(result));
         }
 
-        [TestCase(2, 2,2,6)]
-        public void AcumilatorAdd(int a, int b, int c,int result)
+
+        [TestCase(1, 5)]
+        [TestCase(2,6)]
+        [TestCase(5, 9)]
+        [TestCase(10, 14)]
+        public void AcumilatorAdd(int c,int result)
         {
             _uut.Accumulator = 4;
             Assert.That(_uut.AccumulatorAdd(c), Is.EqualTo(result));
         }
+
+        [TestCase(1, 9)]
+        [TestCase(2, 8)]
+        [TestCase(5, 5)]
+        [TestCase(9, 1)]
+        public void AcumilatorSub(int c, int result)
+        {
+            _uut.Accumulator = 10;
+            Assert.That(_uut.AccumulatorSub(c), Is.EqualTo(result));
+        }
+
+
+        [TestCase(2, 10)]
+        [TestCase(3, 15)]
+        [TestCase(5, 25)]
+        [TestCase(9, 95)]
+        public void AcumilatorMulti(int c, int result)
+        {
+            _uut.Accumulator = 5;
+            Assert.That(_uut.AccumulatorMulti(c), Is.EqualTo(result));
+        }
+
+
+
+
 
         [TestCase()]
         public void ClearAcumilator()
